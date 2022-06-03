f1823ce8173d6b71daee72e346ff39d51b676b05
Renan Paiva <renanvsp@hotmail.com>
2022-06-03 10:40:36 -0300

    iniciando models

---
+++ backend/src/main/java/br/com/treinaweb/adoteumpet/core/models/pet.java
@@ -0,0 +1,85 @@
+package br.com.treinaweb.adoteumpet.core.models;
+
+public class Pet {
+    
+    private Long id;
+
+    private String nome;
+
+    private String historia;
+
+    private String foto;
+
+    public Pet() {}
+
+    public Pet(Long id, String nome, String historia, String foto) {
+        this.id = id;
+        this.nome = nome;
+        this.historia = historia;
+        this.foto = foto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Pet other = (Pet) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Pet [foto=" + foto + ", historia=" + historia + ", id=" + id + ", nome=" + nome + "]";
+    }
+
+    
+}
