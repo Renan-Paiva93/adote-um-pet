d00e8b464b006207524bb364d4120c656db3b01a
Renan Paiva <renanvsp@hotmail.com>
2022-06-03 15:49:30 -0300

    action de listagem de Pets

--- backend/src/main/java/br/com/treinaweb/adoteumpet/api/pet/controllers/PetController.java
+++ backend/src/main/java/br/com/treinaweb/adoteumpet/api/pet/controllers/PetController.java
@@ -1,8 +1,24 @@
 package br.com.treinaweb.adoteumpet.api.pet.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.treinaweb.adoteumpet.core.models.Pet;
+import br.com.treinaweb.adoteumpet.core.repositories.PetRepository;
 
 @RestController
 public class PetController {
-    
+   
+    @Autowired
+    private PetRepository petRepository;
+
+    @GetMapping("/pets")
+    public List<Pet> findAll() {
+        return petRepository.findAll();
+    }
+
+
 }
