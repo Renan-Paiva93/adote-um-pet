0b7e10a1e0d7090e2c6efa1b7da271e9c90cf0a8
Renan Paiva <renanvsp@hotmail.com>
2022-06-03 11:42:53 -0300

    criação model

--- backend/src/main/java/br/com/treinaweb/adoteumpet/core/models/pet.java
+++ backend/src/main/java/br/com/treinaweb/adoteumpet/core/models/pet.java
@@ -1,85 +1,35 @@
 package br.com.treinaweb.adoteumpet.core.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Pet {
     
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
+    @Column(nullable = false)
     private String historia;
 
+    @Column(nullable = false)
     private String foto;
 
-    public Pet() {}
-
-    public Pet(Long id, String nome, String historia, String foto) {
-        this.id = id;
-        this.nome = nome;
-        this.historia = historia;
-        this.foto = foto;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getHistoria() {
-        return historia;
-    }
-
-    public void setHistoria(String historia) {
-        this.historia = historia;
-    }
-
-    public String getFoto() {
-        return foto;
-    }
-
-    public void setFoto(String foto) {
-        this.foto = foto;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Pet other = (Pet) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Pet [foto=" + foto + ", historia=" + historia + ", id=" + id + ", nome=" + nome + "]";
-    }
-
-    
 }
