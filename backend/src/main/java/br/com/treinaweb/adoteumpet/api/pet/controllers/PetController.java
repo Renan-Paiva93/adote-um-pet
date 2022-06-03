26c87a51ab41042ff7281e05198e4582f058c326
Renan Paiva <renanvsp@hotmail.com>
2022-06-03 17:22:17 -0300

    dto e mappers de Pets

--- backend/src/main/java/br/com/treinaweb/adoteumpet/api/pet/controllers/PetController.java
+++ backend/src/main/java/br/com/treinaweb/adoteumpet/api/pet/controllers/PetController.java
@@ -1,11 +1,14 @@
 package br.com.treinaweb.adoteumpet.api.pet.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.treinaweb.adoteumpet.api.pet.dtos.PetResponse;
+import br.com.treinaweb.adoteumpet.api.pet.mappers.PetMapper;
 import br.com.treinaweb.adoteumpet.core.models.Pet;
 import br.com.treinaweb.adoteumpet.core.repositories.PetRepository;
 
@@ -15,10 +18,16 @@ public class PetController {
     @Autowired
     private PetRepository petRepository;
 
+    @Autowired
+    private PetMapper petMapper;
+
     @GetMapping("/pets")
-    public List<Pet> findAll() {
-        return petRepository.findAll();
+    public List<PetResponse> findAll() {
+        var pets = petRepository.findAll();
+        var petResponses = new ArrayList<PetResponse>();
+        for(Pet pet: pets) {
+            petResponses.add(petMapper.toResponse(pet));
+        }
+        return petResponses;
     }
-
-
 }
