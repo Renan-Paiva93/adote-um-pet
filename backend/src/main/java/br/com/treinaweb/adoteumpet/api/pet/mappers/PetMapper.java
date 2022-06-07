3a3990d322fe931ad01aa3bad9dbd293b8e996f4
Renan Paiva <renanvsp@hotmail.com>
2022-06-07 09:59:34 -0300

    Builder nos Mappers finalizado

--- backend/src/main/java/br/com/treinaweb/adoteumpet/api/pet/mappers/PetMapper.java
+++ backend/src/main/java/br/com/treinaweb/adoteumpet/api/pet/mappers/PetMapper.java
@@ -9,12 +9,13 @@ import br.com.treinaweb.adoteumpet.core.models.Pet;
 public class PetMapper {
     
   public PetResponse toResponse(Pet pet) {
-      var petResponse = new PetResponse();
-     petResponse.setId(pet.getId());
-     petResponse.setNome(pet.getNome());
-     petResponse.setHistoria(pet.getHistoria());
-     petResponse.setFoto(pet.getFoto());
-     return petResponse;
+    return PetResponse.builder()
+    .id(pet.getId())
+    .nome(pet.getNome())
+    .historia(pet.getHistoria())
+    .foto(pet.getFoto())
+    .build();
+
   }
 
 }
